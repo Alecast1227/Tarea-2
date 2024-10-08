c296787ed048396d8495e5f076865ab517ed13a7
Alecast1227 <castilloarroyoalejandro@gmail.com>
2024-10-07 19:20:42 -0600

    Tarea 2
    Tarea 2

---
+++ jjk/src/main/java/com/jjk/JjkApplication.java
@@ -0,0 +1,13 @@
+package com.jjk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JjkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JjkApplication.class, args);
+	}
+
+}
---
+++ jjk/src/test/java/com/jjk/JjkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jjk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JjkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
